776685c34a62e3f36786502b2c0d9f7ce1ba3c7a
Rom1 <romain.cernon@mail.com>
2023-12-23 17:42:05 +0100

    connexion

--- main.py
+++ main.py
@@ -4,16 +4,28 @@ import pandas as pd
 # Chargement des données
 stations = pd.read_csv('rere.csv', sep=';')
 
-
 # Création du graphique
 plt.figure(figsize=(10, 8))
-for ligne in stations['ligne'].unique():
-    ligne_data = stations[stations['ligne'] == ligne]
-    plt.plot(ligne_data['x'], ligne_data['y'], marker='o', label=ligne)
+
+# Tracer les stations
+for index, row in stations.iterrows():
+    plt.plot(row['x'], row['y'], marker='o', color='purple')
+
+
+
+
+for index, row in stations.iterrows():
+    connexion = row['connexion']
+    if not pd.isna(connexion):
+        station_connectee = stations[stations['id'] == connexion].iloc[0]
+        plt.plot([row['x'], station_connectee['x']], [row['y'], station_connectee['y']], color='purple')
+
+
 
 plt.title('Carte des Stations de RER à Paris')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
-plt.legend()
+plt.legend("E", fontsize=12)
 plt.show()
+
 
