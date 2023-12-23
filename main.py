9e2d7fcac2bd1c8ef322d2ac1c91903590623b1f
Rom1 <romain.cernon@mail.com>
2023-12-23 16:55:54 +0100

    affichage rer E

--- main.py
+++ main.py
@@ -1,23 +1,19 @@
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
 
 # Chargement des données
-data = {
-    'nom': ['Station A', 'Station B', 'Station C', 'Station D', 'Station E', 'Station F'],
-    'ligne': ['A', 'A', 'A', 'B', 'B', 'C'],
-    'x': np.random.rand(6),
-    'y': np.random.rand(6)
-}
-stations = pd.DataFrame(data)
+stations = pd.read_csv('rere.csv', sep=';')
+
+
 # Création du graphique
 plt.figure(figsize=(10, 8))
 for ligne in stations['ligne'].unique():
     ligne_data = stations[stations['ligne'] == ligne]
-    plt.plot(ligne_data['x'], ligne_data['y'], marker='o', label=f'Ligne {ligne}')
+    plt.plot(ligne_data['x'], ligne_data['y'], marker='o', label=ligne)
 
 plt.title('Carte des Stations de RER à Paris')
 plt.xlabel('Longitude')
 plt.ylabel('Latitude')
 plt.legend()
 plt.show()
+
